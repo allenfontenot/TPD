b65a47781ab4314cd98c524334dcb023ad8f4673
Allen <allen.fontenot@gmail.com>
2015-12-10 06:54:11 -0600

    moved contents of cronscripts to main directory

--- logbackup.sh
+++ logbackup.sh
@@ -7,7 +7,7 @@ cd /home/pi/TPD/logs
 sudo touch debug.log
 sudo touch violations.csv
 
-cd /home/pi/TPD/DU
+cd /home/pi/TPD
 
 sudo ./dropbox_uploader.sh -s -f .duconfig upload /home/pi/TPD/logs/TPDlogs/*.csv /TPDlogs-gehrig/
 
