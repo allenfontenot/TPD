5a2a5ae6e96d368fe135e1afd253283e8bf312a8
Allen <allen.fontenot@gmail.com>
2015-12-10 07:34:49 -0600

    bugsssssssss

--- logbackup.sh
+++ logbackup.sh
@@ -9,8 +9,8 @@ sudo touch violations.csv
 
 cd /home/pi/TPD
 
-sudo ./dropbox_uploader.sh -s -f .duconfig upload /home/pi/TPD/logs/TPDlogs/*.csv /TPDlogs-gehrig/
+sudo bash dropbox_uploader.sh -s -f .duconfig upload /home/pi/TPD/logs/TPDlogs/*.csv /TPDlogs-gehrig/
 
-sudo ./dropbox_uploader.sh -s -f .duconfig upload /home/pi/TPD/logs/debuglogs/*.txt /debuglogs-gehrig/
+sudo bash dropbox_uploader.sh -s -f .duconfig upload /home/pi/TPD/logs/debuglogs/*.txt /debuglogs-gehrig/
 
-sudo ./dropbox_uploader.sh -s -f .duconfig upload /home/pi/TPD/logs/violations/*.csv /violations-gehrig/
+sudo bash dropbox_uploader.sh -s -f .duconfig upload /home/pi/TPD/logs/violations/*.csv /violations-gehrig/
