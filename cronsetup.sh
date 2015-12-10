45f10e7279641d0e6b93345aafbc4d3cdc793177
Allen <allen.fontenot@gmail.com>
2015-12-10 06:56:48 -0600

    bugsssssssss

--- cronsetup.sh
+++ cronsetup.sh
@@ -11,6 +11,6 @@ sudo cp /home/pi/TPD/Crons/isRunningCheck /etc/cron.d/isRunningCheck
 sudo chmod +x /etc/cron.d/dashonrestart
 sudo chmod +x /etc/cron.d/logbackup
 sudo chmod +x /etc/cron.d/isRunningCheck
-sudo chmod +x /home/pi/TPD/DU/dropbox_uploader.sh
+sudo chmod +x /home/pi/TPD/dropbox_uploader.sh
 
 sudo python simtp.py
