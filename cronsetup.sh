884b35548b7ea0393925553e38f5b413a24543ea
allenfontenot <allen.fontenot@gmail.com>
2015-12-22 06:41:34 -0600

    Update cronsetup.sh

--- cronsetup.sh
+++ cronsetup.sh
@@ -5,15 +5,18 @@ sudo chmod +x /home/pi/TPD/logbackup.sh
 sudo chmod +x /home/pi/TPD/Running_Check.py
 sudo chmod +x /home/pi/TPD/Running_Check.sh
 sudo chmod +x /home/pi/TPD/simtp.py
+sudo chmod +x /home/pi/TPD/amreboot.sh
 
 
 sudo cp /home/pi/TPD/Crons/dashonrestart /etc/cron.d/dashonrestart
 sudo cp /home/pi/TPD/Crons/logbackup /etc/cron.d/logbackup
 sudo cp /home/pi/TPD/Crons/isRunningCheck /etc/cron.d/isRunningCheck
+sudo cp /home/pi/TPD/Crons/amreboot /etc/cron.d/isRunningCheck/amreboot
 
 sudo chmod +x /etc/cron.d/dashonrestart
 sudo chmod +x /etc/cron.d/logbackup
 sudo chmod +x /etc/cron.d/isRunningCheck
+sudo chmod +x /etc/cron.d/amreboot
 sudo chmod +x /home/pi/TPD/dropbox_uploader.sh
 
 sudo python simtp.py
