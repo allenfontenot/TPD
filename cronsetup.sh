1534081041160fb5e6325adac83b7b0e11ff4f52
allenfontenot <allen.fontenot@gmail.com>
2015-12-22 10:01:21 -0600

    Update cronsetup.sh

--- cronsetup.sh
+++ cronsetup.sh
@@ -11,7 +11,7 @@ sudo chmod +x /home/pi/TPD/amreboot.sh
 sudo cp /home/pi/TPD/Crons/dashonrestart /etc/cron.d/dashonrestart
 sudo cp /home/pi/TPD/Crons/logbackup /etc/cron.d/logbackup
 sudo cp /home/pi/TPD/Crons/isRunningCheck /etc/cron.d/isRunningCheck
-sudo cp /home/pi/TPD/Crons/amreboot /etc/cron.d/isRunningCheck/amreboot
+sudo cp /home/pi/TPD/Crons/amreboot /etc/cron.d/amreboot
 
 sudo chmod +x /etc/cron.d/dashonrestart
 sudo chmod +x /etc/cron.d/logbackup
