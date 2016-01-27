05c92aa0d1122b4781a8a17f6b5d2fdee659fca5
allenfontenot <allen.fontenot@gmail.com>
2016-01-27 08:58:46 -0600

    nightlyNumbers daily working added crons and updated cronsetup

--- cronsetup.sh
+++ cronsetup.sh
@@ -6,17 +6,21 @@ sudo chmod +x /home/pi/TPD/Running_Check.py
 sudo chmod +x /home/pi/TPD/Running_Check.sh
 sudo chmod +x /home/pi/TPD/simtp.py
 sudo chmod +x /home/pi/TPD/amreboot.sh
+sudo chmod +x /home/pi/TPD/nightlyNumbers.sh
+sudo chmod +x /home/pi/TPD/nightlyNumbers.py
 
 
 sudo cp /home/pi/TPD/Crons/dashonrestart /etc/cron.d/dashonrestart
 sudo cp /home/pi/TPD/Crons/logbackup /etc/cron.d/logbackup
 sudo cp /home/pi/TPD/Crons/isRunningCheck /etc/cron.d/isRunningCheck
 sudo cp /home/pi/TPD/Crons/amreboot /etc/cron.d/amreboot
+sudo cp /home/pi/TPD/Crons/nightlyNumbers /etc/cron.d/nightlyNumbers
 
 sudo chmod +x /etc/cron.d/dashonrestart
 sudo chmod +x /etc/cron.d/logbackup
 sudo chmod +x /etc/cron.d/isRunningCheck
 sudo chmod +x /etc/cron.d/amreboot
+sudo chmod +x /etc/cron.d/nightlyNumbers
 sudo chmod +x /home/pi/TPD/dropbox_uploader.sh
 
 sudo python simtp.py
--- nightlyNumbers.py
+++ nightlyNumbers.py
@@ -4,9 +4,9 @@ from background import *
 import json
 import httplib
 #from settings import NSN
-from settings import NSNlist
 
-NSN = '10841'
+
+NSN = 10841
 
 ###GET VALUES FROM LOG###
 violation = 0
@@ -14,27 +14,19 @@ average = 0
 firstTravelPath = ""
 lastTravelPath = ""
 
-
 ##get average##
 zd1 = []
 bb = []
 with open('logs/travellog.csv') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in reader:
-        print row
         a = int(row[2])
-        print 'a=' + str(a)
         b = row[0]
-        print 'b=' + str(b)
         zd1.append(a)
         bb.append(b)
 
-        print zd1
-        print bb
 zd2 = filter(lambda aa: aa != 0, zd1)  # takes out the zeroes
-
 average = sum(zd2) / len(zd2)
-
 lastTravelPath = bb[(len(bb)-1)]
 firstTravelPath = bb[7]
 ###############
@@ -51,51 +43,24 @@ with open('logs/violations.csv') as csvfile:
 
 connection = httplib.HTTPSConnection('api.parse.com', 443)
 
-weeklyObjectIdList = ["H4nYULUo31","griFoSEZDk","EjmReTVEvl","8zxVdPEa8I","MWZaLsi24R","mHW0jhUSEV","fKKKGVjwUS","plP4YQWBns","qvsOip2hwb","FXfxglBaLJ"]
-yesterdayObjectIdList = ["5i1tLcJEZH","E7dWjWHJHs","S7f2OaNzEZ","LlXnnbSj6L","OuLXZ6mx2e","3KlKMMfH4f","23frTQgJLg","8bCL5w9QYM","2UPvz6P78c","JPfeqFuG2Z"]
-monthlyObjectIdList = ["FRhJlsyEHx","TPcC63O2T8","LIa8VtuiNr","TaSlFH90nJ","FU4HTADmh7","YyXrEaWqj2","R6gU8qBtdm","Fp5j3faFQe","Nu5LbTejmM","XosQXKIoCS"]
-
-weeklyObjectId = weeklyObjectIdList[NSNlist.index(NSN)]
-yesterdayObjectId = yesterdayObjectIdList[NSNlist.index(NSN)]
-monthlyObjectId = monthlyObjectIdList[NSNlist.index(NSN)]
-
-
-weeklyObjectString = '/1/classes/weeklyNumbers/' + str(weeklyObjectId)
-yesterdayObjectString = '/1/classes/yesterdayNumbers/' + str(yesterdayObjectId)
-mothlyObjectString = '/1/classes/monthlyNumbers/' + str(monthlyObjectId)
+#create new objects instead of updating
 try:
     connection.connect()
-    connection.request('PUT', yesterdayObjectString,
-                       json.dumps({"violation": violation}),
-                       {"X-Parse-Application-Id": "BupCLnOBroEGuXa9qkAWebNSzT0o18MTUQeXNJXO",
-                        "X-Parse-REST-API-Key": "oSYBMvF2ET9RvyWhxnIpYa27rObd4XATJoh9zueh",
-                        "Content-Type": "application/json"})
-    result = json.loads(connection.getresponse().read())
-    logging.debug(result)
-    connection.request('PUT', yesterdayObjectString,
-                       json.dumps({"average": average}),
-                       {"X-Parse-Application-Id": "BupCLnOBroEGuXa9qkAWebNSzT0o18MTUQeXNJXO",
-                        "X-Parse-REST-API-Key": "oSYBMvF2ET9RvyWhxnIpYa27rObd4XATJoh9zueh",
-                        "Content-Type": "application/json"})
-    result = json.loads(connection.getresponse().read())
-    logging.debug(result)
-
-    connection.request('PUT', yesterdayObjectString,
-                       json.dumps({"lastTravelPath": lastTravelPath}),
-                       {"X-Parse-Application-Id": "BupCLnOBroEGuXa9qkAWebNSzT0o18MTUQeXNJXO",
-                        "X-Parse-REST-API-Key": "oSYBMvF2ET9RvyWhxnIpYa27rObd4XATJoh9zueh",
-                        "Content-Type": "application/json"})
-    result = json.loads(connection.getresponse().read())
-    logging.debug(result)
-    connection.request('PUT', yesterdayObjectString,
-                       json.dumps({"firstTravelPath": firstTravelPath}),
-                       {"X-Parse-Application-Id": "BupCLnOBroEGuXa9qkAWebNSzT0o18MTUQeXNJXO",
-                        "X-Parse-REST-API-Key": "oSYBMvF2ET9RvyWhxnIpYa27rObd4XATJoh9zueh",
-                        "Content-Type": "application/json"})
-    result = json.loads(connection.getresponse().read())
-    logging.debug(result)
+    connection.request('POST', '/1/classes/yesterdayNumbers', json.dumps({
+           "store": NSN,
+           "violation": violation,
+           "average": average,
+           "firstTravelPath": firstTravelPath,
+           "lastTravelPath": lastTravelPath
+         }), {
+           "X-Parse-Application-Id": "BupCLnOBroEGuXa9qkAWebNSzT0o18MTUQeXNJXO",
+           "X-Parse-REST-API-Key": "oSYBMvF2ET9RvyWhxnIpYa27rObd4XATJoh9zueh",
+           "Content-Type": "application/json"
+         })
+    results = json.loads(connection.getresponse().read())
+    logging.debug(results)
 
 except Exception:
+    print "Connection Failed"
     logging.debug("Parse Connection Failed")
     pass
-
