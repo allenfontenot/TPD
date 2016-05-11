2bde681a50fd29692c5f954567546a51284c23ba
allenfontenot <allen.fontenot@gmail.com>
2016-05-11 13:53:51 -0500

    Update cronsetup.sh

--- cronsetup.sh
+++ cronsetup.sh
@@ -8,13 +8,14 @@ sudo chmod +x /home/pi/TPD/simtp.py
 sudo chmod +x /home/pi/TPD/amreboot.sh
 sudo chmod +x /home/pi/TPD/nightlyNumbers.sh
 sudo chmod +x /home/pi/TPD/nightlyNumbers.py
-
+update_emails
 
 sudo cp /home/pi/TPD/Crons/dashonrestart /etc/cron.d/dashonrestart
 sudo cp /home/pi/TPD/Crons/logbackup /etc/cron.d/logbackup
 sudo cp /home/pi/TPD/Crons/isRunningCheck /etc/cron.d/isRunningCheck
 sudo cp /home/pi/TPD/Crons/amreboot /etc/cron.d/amreboot
 sudo cp /home/pi/TPD/Crons/nightlyNumbers /etc/cron.d/nightlyNumbers
+sudo cp /home/pi/TPD/Crons/update_emails /etc/cron.d/update_emails
 
 sudo chmod +x /etc/cron.d/dashonrestart
 sudo chmod +x /etc/cron.d/logbackup
@@ -22,5 +23,6 @@ sudo chmod +x /etc/cron.d/isRunningCheck
 sudo chmod +x /etc/cron.d/amreboot
 sudo chmod +x /etc/cron.d/nightlyNumbers
 sudo chmod +x /home/pi/TPD/dropbox_uploader.sh
+sudo chmod +x /home/pi/TPD/update_emails
 
 sudo python simtp.py
