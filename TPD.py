edaf23dcc7e1b50d673f1b5d8fcabd5b8e1d7509
allenfontenot <allen.fontenot@gmail.com>
2016-05-11 14:05:15 -0500

    Update TPD.py

--- TPD.py
+++ TPD.py
@@ -87,7 +87,7 @@ while True:  # Main Loop contains 1 loop for offline and 1 loop for online
             sendmail("online", 0, 0, NSN, 5)
         except socket.gaierror:
             logging.debug(str(timenow()) + ' email failed')
-         except SMTPRecipientsRefused:
+        except SMTPRecipientsRefused:
                 logging.debug(str(timenow()) + ' email failed SMTPRecipientsRefused')
 
         onlineFirstRun = False
